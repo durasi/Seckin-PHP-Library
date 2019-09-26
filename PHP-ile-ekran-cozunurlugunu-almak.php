1f65570f34fa2eabe75a4d345608ba8e93154b54
Seçkin Durası <seckin24@windowslive.com>
2019-09-26 12:46:25 +0300

    PHP ile ekran çözünürlüğünü almak

---
+++ PHP-ile-ekran-cozunurlugunu-almak.php
@@ -0,0 +1,20 @@
+<?
+//2019-09-26
+//www.seckin.co
+//Seçkin Durası 
+//https://github.com/durasi/PHPKutuphanesi
+
+    if(!isset($_GET['resolution'])) {     
+        echo "<script language=\"JavaScript\">     
+<!--      
+    document.location=\"$PHP_SELF?resolution=1&width=\"+screen.width+\"&height=\"+screen.height;     
+//-->     
+</script>";     
+    } else {
+        if(isset($_GET['width']) && isset($_GET['height'])) {     
+            echo "Width: " . $_GET['width'] . " and Height: " . $_GET['height'] . "<br />";
+        } else {     
+            echo "Çözünürlük algılanmadı.";
+        }     
+    }
+?>
