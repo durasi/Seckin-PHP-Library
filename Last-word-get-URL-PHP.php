e23646d651deb81aa872837f218163b5fd656533
Seçkin Durası <bilgi@seckin.co>
2021-03-13 14:21:22 +0300

    Last-word-get-URL-PHP

---
+++ Last-word-get-URL-PHP.php
@@ -0,0 +1,14 @@
+<!--
+//2021-03-13
+//www.seckin.co
+//Seçkin Durası 
+//https://github.com/durasi/Seckin-PHP-Library
+-->
+
+
+
+<?
+
+	$url = end(explode('/', trim($_SERVER['REQUEST_URI'], '/')));
+
+?>
