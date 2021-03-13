91c9cf0527c632ae12b075a3e2ae55c8afd05fc4
Seçkin Durası <bilgi@seckin.co>
2021-03-13 13:19:23 +0300

    Update PHP-Web-Site-Base-Kodu.php

--- PHP-Web-Site-Base-Kodu.php
+++ PHP-Web-Site-Base-Kodu.php
@@ -2,6 +2,6 @@
 //2019-11-14
 //www.seckin.co
 //Seçkin Durası 
-//https://github.com/durasi/PHP-Kutuphanesi
+//https://github.com/durasi/Seckin-PHP-Library
 -->
 <base href="https://www.seckin.co"/>
